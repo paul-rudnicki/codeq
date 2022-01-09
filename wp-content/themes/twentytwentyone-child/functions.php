f3a57d4b16f315d000ae5f940a4753cbcaf42811
Paweł Rudnicki <pawel.rudnicki@lama-media.com>
2022-01-09 13:49:13 +0100

    figma tempate page

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -1,7 +1,9 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'codeq_enqueue_styles', 11);
+add_action( 'wp_enqueue_scripts', 'codeq_enqueue_styles', 20);
 function codeq_enqueue_styles() {
+    wp_dequeue_style('twenty-twenty-one-style');
+    wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', );
     wp_enqueue_style( 'codeq-style', get_bloginfo( 'stylesheet_directory' ) . '/dist/css/styles.min.css', );
     wp_enqueue_script( 'codeq-script', get_bloginfo( 'stylesheet_directory' ) . '/dist/js/script.min.js', array(), '1.0.0', true );
 }
